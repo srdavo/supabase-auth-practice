80e876db0e1416839b858c3faa75800e1056c512
srdavo <davoluis11@gmail.com>
2025-12-01 13:33:30 -0700

    feat: Setup React + Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Home(){
+	return <h1>Home</h1>;
+}
+
+export default function App() {
+	return (
+		<div>
+			<h1>Supabase Auth + React</h1>
+			<p>Hola</p>
+		</div>
+	);
+}
